fa6086a76a02c1965bb5ee2306c60fe48acdb0cb
woody35545 <woody35545@gmail.com>
2024-03-28 14:54:46 +0900

    feat: #8 `Packaging` - Comment 추가

--- src/main/java/com/t3t/bookstoreapi/order/model/entity/Packaging.java
+++ src/main/java/com/t3t/bookstoreapi/order/model/entity/Packaging.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+/**
+ * 책 포장지 엔티티
+ * @Author woody35545(구건모)
+ */
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,12 +23,15 @@ public class Packaging {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "package_id")
+    @Comment("포장지 식별자")
     private Long id;
 
     @Column(name = "package_name", length = 50, nullable = false)
+    @Comment("포장지 이름")
     private String name;
 
     @Column(name = "package_price", nullable = false)
+    @Comment("포장지 단가")
     private BigDecimal price;
 
 }
