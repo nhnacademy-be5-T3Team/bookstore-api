0fc0eb8f51e1889816c8a9be99390f92e0cea2d4
yujinKim <yujinkimvv@gmail.com>
2024-03-29 00:53:48 +0900

    refactor : #17 Tag Entity Builder 추가

--- src/main/java/com/t3t/bookstoreapi/tag/entity/Tag.java
+++ src/main/java/com/t3t/bookstoreapi/tag/entity/Tag.java
@@ -1,10 +1,14 @@
 package com.t3t.bookstoreapi.tag.entity;
 
 import com.sun.istack.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Table(name = "tags")
@@ -18,4 +22,9 @@ public class Tag {
     @NotNull
     @Column(name = "tag_name")
     private String tagName;
+
+    @Builder
+    public Tag(String tagName) {
+        this.tagName = tagName;
+    }
 }
