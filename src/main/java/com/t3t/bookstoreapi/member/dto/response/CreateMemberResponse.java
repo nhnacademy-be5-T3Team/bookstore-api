9efcc86b9e66de659f3842a9c20a70129cb10f0f
PyeonMinjun <alswns9810@gmail.com>
2024-03-29 14:17:50 +0900

    fix : Member 레포 수정

---
+++ src/main/java/com/t3t/bookstoreapi/member/dto/response/CreateMemberResponse.java
@@ -0,0 +1,11 @@
+package com.t3t.bookstoreapi.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateMemberResponse {
+
+    private Long memberId;
+}
