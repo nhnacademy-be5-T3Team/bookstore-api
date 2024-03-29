a3b7db186ab016f2dad4479fedde1958a389c5cb
woody35545 <woody35545@gmail.com>
2024-03-29 12:56:46 +0900

    feat: #21 `MemberGrade` - 회원 등급 엔티티 맵핑

---
+++ src/main/java/com/t3t/bookstoreapi/member/domain/MemberGrade.java
@@ -0,0 +1,31 @@
+package com.t3t.bookstoreapi.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.*;
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name = "member_grades")
+public class MemberGrade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_grade_id", columnDefinition = "TINYINT")
+    @Comment("회원 등급 식별자")
+    private int gradeId;
+
+    @Column(name = "member_grade_name", length = 30, nullable = false)
+    @Comment("회원 등급 이름")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "member_grade_policy_id")
+    @Comment("회원 등급 정책 식별자")
+    private MemberGradePolicy policy;
+}
