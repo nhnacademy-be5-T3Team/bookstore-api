623f8b48d42ed1bc43f8b56bb721ebf54ee300f9
parkjonggyeong18 <gd122572@gmail.com>
2024-03-29 16:41:31 +0900

    refactor: #13 결제처 repository 수정

--- src/main/java/com/t3t/bookstoreapi/payment/repository/PaymentProviderRepository.java
+++ src/main/java/com/t3t/bookstoreapi/payment/repository/PaymentProviderRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentProviderRepository extends JpaRepository<PaymentProvider, Long> {
+public interface PaymentProviderRepository extends JpaRepository<PaymentProvider, Integer> {
     PaymentProvider findByPaymentProviderName(String paymentProviderName);
 }
