c3041218441ee09e93cdc78c66580bbdd376b285
woody35545 <woody35545@gmail.com>
2024-03-29 13:12:02 +0900

    feat: #21 `Member` - 회원 엔티티 맵핑

--- src/main/java/com/t3t/bookstoreapi/member/domain/Member.java
+++ src/main/java/com/t3t/bookstoreapi/member/domain/Member.java
@@ -1,8 +1,9 @@
 package com.t3t.bookstoreapi.member.domain;
 
 import lombok.Getter;
-
+import org.hibernate.annotations.Comment;
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,34 +14,43 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
+    @Comment("회원 식별자")
     private Long id;
 
-
-    @JoinColumn(name = "grade_id")
     @ManyToOne
-    private Grade gradeId;
+    @JoinColumn(name = "member_grade_id", nullable = false)
+    @Comment("회원 등급 식별자")
+    private MemberGrade gradeId;
 
-    @Column(name = "member_name")
+    @Column(name = "member_name", length = 100, nullable = false)
+    @Comment("회원 이름")
     private String name;
 
-    @Column(name = "member_phone")
+    @Column(name = "member_phone", length = 13, nullable = false)
+    @Comment("회원 전화번호")
     private String phone;
 
-    @Column(name = "member_email")
+    @Column(name = "member_email", length = 100, nullable = false)
+    @Comment("회원 이메일")
     private String email;
 
-    @Column(name = "member_birthdate")
-    private LocalDateTime birthDate;
+    @Column(name = "member_birthdate", nullable = false)
+    @Comment("회원 생년월일")
+    private LocalDate birthDate;
 
-    @Column(name = "member_latest_login")
+    @Column(name = "member_latest_login", nullable = false)
+    @Comment("회원 최근 로그인 일시")
     private LocalDateTime latestLogin;
 
-    @Column(name = "member_point")
-    private int point;
+    @Column(name = "member_point", nullable = false)
+    @Comment("회원 보유 포인트")
+    private Long point;
 
-    @Column(name = "member_status")
+    @Column(name = "member_status", length = 10)
+    @Comment("회원 상태")
     private String status;
 
-    @Column(name = "member_role")
-    private byte role;
+    @Column(name = "member_role", columnDefinition = "TINYINT", nullable = false)
+    @Comment("회원 역할")
+    private int role;
 }
