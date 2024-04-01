959d24354bed9ce8f673aa0caff5388e4fcd81c7
yujinKim <yujinkimvv@gmail.com>
2024-04-02 01:02:15 +0900

    feat : #32 카테고리 별 도서 검색 목록 조회 api 구현

--- src/main/java/com/t3t/bookstoreapi/book/controller/BookCategoryController.java
+++ src/main/java/com/t3t/bookstoreapi/book/controller/BookCategoryController.java
@@ -1,10 +1,15 @@
 package com.t3t.bookstoreapi.book.controller;
 
+import com.t3t.bookstoreapi.book.model.response.BookSearchResultResponse;
 import com.t3t.bookstoreapi.book.service.BookCategoryService;
+import com.t3t.bookstoreapi.model.response.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookCategoryController {
@@ -16,7 +21,7 @@ public class BookCategoryController {
     }
 
     @GetMapping("/categories/{categoryId}/books")
-    public void getBooksByCategoryId(@PathVariable Integer categoryId) {
-        bookCategoryService.findBooksByCategoryId(categoryId);
+    public BaseResponse<List<BookSearchResultResponse>> getBooksByCategoryId(@PathVariable Integer categoryId) {
+        return BaseResponse.success("message", bookCategoryService.findBooksByCategoryId(categoryId));
     }
 }
--- src/main/java/com/t3t/bookstoreapi/book/service/BookCategoryService.java
+++ src/main/java/com/t3t/bookstoreapi/book/service/BookCategoryService.java
@@ -3,6 +3,7 @@ package com.t3t.bookstoreapi.book.service;
 import com.t3t.bookstoreapi.book.model.entity.Book;
 import com.t3t.bookstoreapi.book.model.entity.BookCategory;
 import com.t3t.bookstoreapi.book.model.entity.ParticipantRoleRegistration;
+import com.t3t.bookstoreapi.book.model.response.AuthorInfo;
 import com.t3t.bookstoreapi.book.model.response.BookSearchResultResponse;
 import com.t3t.bookstoreapi.book.repository.BookCategoryRepository;
 import com.t3t.bookstoreapi.book.repository.BookRepository;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,8 +26,7 @@ public class BookCategoryService {
         this.bookRepository = bookRepository;
     }
 
-
-    public void findBooksByCategoryId(Integer categoryId) {
+    public List<BookSearchResultResponse> findBooksByCategoryId(Integer categoryId) {
         // 특정 카테고리 ID에 해당하는 BookCategory를 조회
         List<BookCategory> bookCategories = bookCategoryRepository.findByCategoryCategoryId(categoryId);
 
@@ -35,31 +36,45 @@ public class BookCategoryService {
                 .map(Book::getBookId)
                 .collect(Collectors.toList());
 
+        // 도서 ID에 해당하는 도서 데이터 조회
         List<Book> books = bookRepository.findByBookIdIn(bookIds);
 
-        List<BookSearchResultResponse> responses = null;
+        List<BookSearchResultResponse> responses = new ArrayList<>();
 
+        // 도서 데이터 순회하면서 연관 정보 조회
         for (Book book : books) {
+            List<ParticipantRoleRegistration> authorList = book.getAuthors();
 
-            List<ParticipantRoleRegistration> list = book.getAuthors();
+            List<AuthorInfo> authorInfoList = new ArrayList<>();
 
-            BookSearchResultResponse.builder()
+            for (ParticipantRoleRegistration participantRole : authorList) {
+                AuthorInfo author = AuthorInfo.builder()
+                        .role(participantRole.getParticipantRole().getParticipantRoleNameKr())
+                        .name(participantRole.getParticipant().getParticipantName())
+                        .build();
+
+                authorInfoList.add(author);
+            }
+
+            BookSearchResultResponse result = BookSearchResultResponse.builder()
                     .name(book.getBookName())
-                    .published(book.getBookPublished())
                     .price(book.getBookPrice())
-                    .discount(calculateDiscountedPrice(book.getBookPrice(), book.getBookDiscount()))
+                    .discountRate(book.getBookDiscount())
+                    .discountedPrice(calculateDiscountedPrice(book.getBookPrice(), book.getBookDiscount()))
+                    .published(book.getBookPublished())
+                    .publisher(book.getPublisher().getPublisherName())
                     .averageScore(book.getBookAverageScore())
                     .likeCount(book.getBookLikeCount())
                     .coverImageUrl(book.getBookThumbnail().getThumbnailImageUrl())
+                    .authorInfoList(authorInfoList)
                     .build();
 
-            List<ParticipantRoleRegistration> authorList = book.getAuthors();
-
-            for(ParticipantRoleRegistration pp : list) {
-
-            }
+            responses.add(result);
         }
+        return responses;
     }
+    
+
     public static BigDecimal calculateDiscountedPrice(BigDecimal originalPrice, BigDecimal discountRate) {
         BigDecimal discountPercentage = discountRate.divide(BigDecimal.valueOf(100));
         BigDecimal discountAmount = originalPrice.multiply(discountPercentage);
