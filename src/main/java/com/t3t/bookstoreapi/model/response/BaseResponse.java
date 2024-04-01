3f0ccd6b0e18eaae61810c1902c707046ae07936
Yujin Kim <67141385+Yujin-nKim@users.noreply.github.com>
2024-04-01 11:14:02 +0900

    style : 불필요한 코드 제거

--- src/main/java/com/t3t/bookstoreapi/model/response/BaseResponse.java
+++ src/main/java/com/t3t/bookstoreapi/model/response/BaseResponse.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 public class BaseResponse<T> {
-    //private int code;
     private final String message;
     private final T data;
 
