e58b0784a9ad733d16ba06c8f6f4556ea39a1fcb
woody35545 <woody35545@gmail.com>
2024-04-03 03:58:56 +0900

    feat: #47 `DeliveryController` - 배송 생성 API

--- src/main/java/com/t3t/bookstoreapi/order/controller/DeliveryController.java
+++ src/main/java/com/t3t/bookstoreapi/order/controller/DeliveryController.java
@@ -2,14 +2,14 @@ package com.t3t.bookstoreapi.order.controller;
 
 import com.t3t.bookstoreapi.model.response.BaseResponse;
 import com.t3t.bookstoreapi.order.model.dto.DeliveryDto;
+import com.t3t.bookstoreapi.order.model.request.DeliveryCreationRequest;
 import com.t3t.bookstoreapi.order.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,8 +19,9 @@ public class DeliveryController {
 
     /**
      * 모든 배송 목록을 조회하는 API
+     *
      * @return 200 OK - 조회된 배송들에 대한 DTO 리스트 반환<br>
-     *                  204 NO_CONTENT - 등록된 배송이 없는 경우 메시지 반환
+     * 204 NO_CONTENT - 등록된 배송이 없는 경우 메시지 반환
      * @author woody35545(구건모)
      */
     @GetMapping("/deliveries")
@@ -34,6 +35,7 @@ public class DeliveryController {
 
     /**
      * 배송 식별자로 배송을 조회하는 API
+     *
      * @param deliveryId 조회하고자 하는 배송 식별자
      * @return 200 OK - 조회된 배송에 대한 DTO 반환
      * @author woody35545(구건모)
@@ -41,5 +43,19 @@ public class DeliveryController {
     @GetMapping("/deliveries/{deliveryId}")
     public ResponseEntity<BaseResponse<DeliveryDto>> getDeliveryById(@PathVariable("deliveryId") long deliveryId) {
         return ResponseEntity.ok(new BaseResponse<DeliveryDto>().data(deliveryService.getDeliveryById(deliveryId)));
+    }
+
+    /**
+     * 배송을 생성하는 API
+     * @param request 생성하고자 하는 배송에 대한 정보를 담은 DTO
+     * @return 201 CREATED - 생성된 배송에 대한 DTO 반환
+     * @author woody35545(구건모)
+     */
+    @PostMapping("/deliveries")
+    public ResponseEntity<BaseResponse<DeliveryDto>> createDelivery(@Valid @RequestBody DeliveryCreationRequest request) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new BaseResponse<DeliveryDto>().data(
+                deliveryService.createDelivery(request.getPrice(), request.getAddressNumber(), request.getRoadnameAddress(), request.getDetailAddress(),
+                        request.getDeliveryDate(), request.getRecipientName(), request.getRecipientPhoneNumber())));
     }
 }
