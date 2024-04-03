e3813436cae2c4be7246d0001925de32f0316805
woody35545 <woody35545@gmail.com>
2024-04-03 13:52:39 +0900

    feat: #52 `GlobalExceptionHandler` - 장바구니 항목 추가 과정에서 발생하는 예외들 대한 핸들러 구현

--- src/main/java/com/t3t/bookstoreapi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/t3t/bookstoreapi/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.t3t.bookstoreapi.common.exception;
 
+import com.t3t.bookstoreapi.book.exception.BookNotFoundException;
 import com.t3t.bookstoreapi.model.response.BaseResponse;
 import com.t3t.bookstoreapi.order.exception.OrderStatusNotFoundException;
 import com.t3t.bookstoreapi.shoppingcart.exception.ShoppingCartNotFoundException;
@@ -40,5 +41,29 @@ public class GlobalExceptionHandler {
     public ResponseEntity<BaseResponse<Void>> handleShoppingCartNotFoundException(ShoppingCartNotFoundException shoppingCartNotFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new BaseResponse<Void>().message(shoppingCartNotFoundException.getMessage()));
+    }
+
+    /**
+     * 책이 존재하지 않는 경우에 대한 예외 처리 핸들러
+     * @param bookNotFoundException 책이 존재하지 않는 경우 발생하는 예외
+     * @return 404 NOT_FOUND - 예외 메시지 반환
+     * @author woody35545(구건모)
+     */
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<BaseResponse<Void>> handleBookNotFoundException(BookNotFoundException bookNotFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new BaseResponse<Void>().message(bookNotFoundException.getMessage()));
+    }
+
+    /**
+     * 잘못된 인자가 전달된 경우에 대한 예외 처리 핸들러
+     * @param illegalArgumentException 잘못된 인자가 전달된 경우 발생하는 예외
+     * @return 400 BAD_REQUEST - 예외 메시지 반환
+     * @author woody35545(구건모)
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<BaseResponse<Void>> handleIllegalArgumentException(IllegalArgumentException illegalArgumentException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new BaseResponse<Void>().message(illegalArgumentException.getMessage()));
     }
 }
