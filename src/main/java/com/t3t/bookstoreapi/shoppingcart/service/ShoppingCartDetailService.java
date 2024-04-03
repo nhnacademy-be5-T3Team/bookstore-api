da7b7cc9939bd15f60297f67d26b5c9a356cfacb
woody35545 <woody35545@gmail.com>
2024-04-03 13:54:46 +0900

    feat: #52 `ShoppingCartDetailService` - 장바구니 항목 추가 기능 구현

--- src/main/java/com/t3t/bookstoreapi/shoppingcart/service/ShoppingCartDetailService.java
+++ src/main/java/com/t3t/bookstoreapi/shoppingcart/service/ShoppingCartDetailService.java
@@ -1,6 +1,11 @@
 package com.t3t.bookstoreapi.shoppingcart.service;
 
+import com.t3t.bookstoreapi.book.exception.BookNotFoundForIdException;
+import com.t3t.bookstoreapi.book.model.entity.Book;
+import com.t3t.bookstoreapi.book.repository.BookRepository;
 import com.t3t.bookstoreapi.shoppingcart.exception.ShoppingCartNotFoundForIdException;
+import com.t3t.bookstoreapi.shoppingcart.model.dto.ShoppingCartDetailDto;
+import com.t3t.bookstoreapi.shoppingcart.model.entity.ShoppingCart;
 import com.t3t.bookstoreapi.shoppingcart.model.entity.ShoppingCartDetail;
 import com.t3t.bookstoreapi.shoppingcart.repository.ShoppingCartDetailRepository;
 import com.t3t.bookstoreapi.shoppingcart.repository.ShoppingCartRepository;
@@ -17,6 +22,7 @@ public class ShoppingCartDetailService {
 
     private final ShoppingCartDetailRepository shoppingCartDetailRepository;
     private final ShoppingCartRepository shoppingCartRepository;
+    private final BookRepository bookRepository;
 
     /**
      * 장바구니 식별자로 전체 항목 리스트 조회
@@ -32,5 +38,28 @@ public class ShoppingCartDetailService {
         }
 
         return shoppingCartDetailRepository.findAllByShoppingCartId(shoppingCartId);
+    }
+
+    /**
+     * 장바구니에 항목 추가
+     * @param shoppingCartId 장바구니 식별자
+     * @param bookId 추가할 책 식별자
+     * @param quantity 추가할 수량
+     * @return 추가된 장바구니 항목에 대한 DTO
+     * @author wooody35545(구건모)
+     */
+    public ShoppingCartDetailDto createShoppingCartDetail(long shoppingCartId, long bookId, long quantity) {
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("수량은 0보다 커야 합니다.");
+        }
+
+        return ShoppingCartDetailDto.of(shoppingCartDetailRepository.save(ShoppingCartDetail.builder()
+                .shoppingCart(shoppingCartRepository.findById(shoppingCartId)
+                        .orElseThrow(() -> new ShoppingCartNotFoundForIdException(shoppingCartId)))
+                .book(bookRepository.findById(bookId)
+                        .orElseThrow(() -> new BookNotFoundForIdException(bookId)))
+                .quantity(quantity)
+                .build()));
     }
 }
