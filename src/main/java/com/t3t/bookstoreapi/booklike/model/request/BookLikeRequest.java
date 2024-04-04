86d79c341b1a494fc6dccddbdd221a6f46c9dcd3
yujinKim <yujinkimvv@gmail.com>
2024-04-04 13:51:34 +0900

    feat : #37 Book entity에 좋아요 수 증가, 감소 메서드 구현

---
+++ src/main/java/com/t3t/bookstoreapi/booklike/model/request/BookLikeRequest.java
@@ -0,0 +1,2 @@
+package com.t3t.bookstoreapi.booklike.model.request;public class BookLikeRequest {
+}
---
+++ src/main/java/com/t3t/bookstoreapi/booklike/service/BookLikeService.java
@@ -0,0 +1,2 @@
+package com.t3t.bookstoreapi.booklike.service;public class BookLikeService {
+}
