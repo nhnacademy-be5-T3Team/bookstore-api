fe144a5d4251beb9b52fc6b322a09642322ab723
yujinKim <yujinkimvv@gmail.com>
2024-04-04 13:55:32 +0900

    feat : #37 회원 id로 회원 찾는 메서드 구현

--- src/main/java/com/t3t/bookstoreapi/member/repository/MemberRepository.java
+++ src/main/java/com/t3t/bookstoreapi/member/repository/MemberRepository.java
@@ -4,7 +4,9 @@ import com.t3t.bookstoreapi.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByName(String name);
+    Optional<Member> findById(Long memberId);
 }
