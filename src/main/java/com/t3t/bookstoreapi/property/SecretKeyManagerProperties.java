2fd6cdb3367128d5326c8c5d345591e91777bf52
woody35545 <woody35545@gmail.com>
2024-04-05 11:14:43 +0900

    modify: #61 `SecretKeyManagerProperties` - local profile 에서는 등록되지 않도록 설정

--- src/main/java/com/t3t/bookstoreapi/property/SecretKeyManagerProperties.java
+++ src/main/java/com/t3t/bookstoreapi/property/SecretKeyManagerProperties.java
@@ -2,6 +2,7 @@ package com.t3t.bookstoreapi.property;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
 /**
@@ -9,7 +10,7 @@ import org.springframework.core.io.Resource;
  * @author woody35545(구건모)
  */
 @Getter
-@Setter
+@Profile("!local")
 @ConfigurationProperties(prefix = "t3t.secret-key-manager")
 public class SecretKeyManagerProperties {
     private String appKey;
