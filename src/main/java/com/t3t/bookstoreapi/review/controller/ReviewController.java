60dbff07d3c9d21437957fbf018cde5a088387a4
yujinKim <yujinkimvv@gmail.com>
2024-04-12 14:13:17 +0900

    fix : 도서별 리뷰 목록 조회 페이징 기능 수정

--- src/main/java/com/t3t/bookstoreapi/review/controller/ReviewController.java
+++ src/main/java/com/t3t/bookstoreapi/review/controller/ReviewController.java
@@ -1,16 +1,18 @@
 package com.t3t.bookstoreapi.review.controller;
 
 import com.t3t.bookstoreapi.model.response.BaseResponse;
+import com.t3t.bookstoreapi.model.response.PageResponse;
 import com.t3t.bookstoreapi.review.model.response.ReviewResponse;
 import com.t3t.bookstoreapi.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,8 +21,25 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @GetMapping("/reviews/book/{bookId}")
-    public ResponseEntity<BaseResponse<Page<ReviewResponse>>> getReviewsByBookId(@PathVariable Long bookId, Pageable pageable) {
-        return ResponseEntity.ok(new BaseResponse<Page<ReviewResponse>>()
-                .data(reviewService.findReviewsByBookId(bookId, pageable)));
+    public ResponseEntity<BaseResponse<PageResponse<ReviewResponse>>> getReviewsByBookId(
+            @PathVariable Long bookId,
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "6", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "reviewCreatedAt", required = false) String sortBy) {
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+
+        PageResponse<ReviewResponse> reviewList = reviewService.findReviewsByBookId(bookId, pageable);
+
+        // 도서에 등록된 리뷰가 없는 경우 | status code 204 (No Content)
+        if (reviewList.getContent().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(
+                    new BaseResponse<PageResponse<ReviewResponse>>().message("도서에 등록된 리뷰가 없습니다.")
+            );
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new BaseResponse<PageResponse<ReviewResponse>>().data(reviewList)
+        );
     }
 }
