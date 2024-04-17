6abcc7b04ba66e2e7bf866e879ad666b0c5c3687
yujinKim <yujinkimvv@gmail.com>
2024-04-17 16:13:19 +0900

    fix : #88 카테고리 전체 목록 조회 api 수정 | 카테고리의 depth 범위를 요청받고 범위에 해당하는 카테고리들을 트리 구조로 반환

--- src/main/java/com/t3t/bookstoreapi/category/controller/CategoryController.java
+++ src/main/java/com/t3t/bookstoreapi/category/controller/CategoryController.java
@@ -1,13 +1,15 @@
 package com.t3t.bookstoreapi.category.controller;
 
-import com.t3t.bookstoreapi.category.model.response.CategoryListResponse;
+import com.t3t.bookstoreapi.category.model.request.CategoryListRequest;
+import com.t3t.bookstoreapi.category.model.response.CategoryTreeResponse;
 import com.t3t.bookstoreapi.category.service.CategoryService;
 import com.t3t.bookstoreapi.model.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -16,8 +18,16 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/categories")
-    public ResponseEntity<BaseResponse<List<CategoryListResponse>>> getCategoriesHierarchy() {
-        return ResponseEntity.ok(new BaseResponse<List<CategoryListResponse>>()
-                .data(categoryService.getCategoriesHierarchy()));
+    public ResponseEntity<BaseResponse<List<CategoryTreeResponse>>> getCategoryTreeByDepth(
+            @Valid @RequestBody CategoryListRequest request) {
+
+        List<CategoryTreeResponse> categoryList = categoryService.getCategoryTreeByDepth(request.getStartDepth(), request.getMaxDepth());
+
+        if(categoryList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(
+                    new BaseResponse<List<CategoryTreeResponse>>().message("등록된 카테고리가 없습니다"));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new BaseResponse<List<CategoryTreeResponse>>().data(categoryList));
     }
 }
--- src/main/java/com/t3t/bookstoreapi/category/repository/CategoryRepository.java
+++ src/main/java/com/t3t/bookstoreapi/category/repository/CategoryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>, CategoryRepositoryCustom {
-    List<Category> findAll();
+    List<Category> findByDepthBetween(int startDepth, int maxDepth);
 }
