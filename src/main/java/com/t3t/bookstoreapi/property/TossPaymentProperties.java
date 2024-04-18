f2bd575ce290a330f000a6567f388e83cdacd799
woody35545 <woody35545@gmail.com>
2024-04-18 09:28:57 +0900

    feat: #92 `TossPaymentPropertiesConfig` - 토스 결제 서비스에서 사용되는 프로퍼티 설정

--- src/main/java/com/t3t/bookstoreapi/property/TossPaymentProperties.java
+++ src/main/java/com/t3t/bookstoreapi/property/TossPaymentProperties.java
@@ -1,11 +1,13 @@
 package com.t3t.bookstoreapi.property;
 
+import lombok.Builder;
 import lombok.Getter;
 
 /**
  * Toss 결제 서비스에서 사용되는 프로퍼티
  * @author woody35545(구건모)
  */
+@Builder
 @Getter
 public class TossPaymentProperties {
     private String widgetSecretKey;
