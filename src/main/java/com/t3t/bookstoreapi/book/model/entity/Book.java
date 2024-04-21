41551bda406309a3bf5c1e9bf0ff121120c5a5ae
yujinKim <yujinkimvv@gmail.com>
2024-04-21 21:00:43 +0900

    fix : #90 Book 엔티티 수정

--- src/main/java/com/t3t/bookstoreapi/book/model/entity/Book.java
+++ src/main/java/com/t3t/bookstoreapi/book/model/entity/Book.java
@@ -10,8 +10,6 @@ import lombok.*;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
