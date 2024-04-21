14ae7add120b94452bb987dc1b10f85b2065704e
woody35545 <woody35545@gmail.com>
2024-04-21 15:34:23 +0900

    feat: #96 `MemberAddressRepositoryCustom` - MemberAddress 엔티티에 대한 커스텀 쿼리를 정의하기 위한 인터페이스

---
+++ src/main/java/com/t3t/bookstoreapi/member/repository/MemberAddressRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.t3t.bookstoreapi.member.repository;
+
+import com.t3t.bookstoreapi.member.model.dto.MemberAddressDto;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * MemberAddress 엔티티에 대한 커스텀 쿼리를 정의하기 위한 인터페이스
+ * @author woody35545(구건모)
+ */
+public interface MemberAddressRepositoryCustom {
+}
