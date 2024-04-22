04b7da6b3064fc10696c74a9d45765d700f064df
yujinKim <yujinkimvv@gmail.com>
2024-04-22 16:59:49 +0900

    test : #98 카테고리 별 도서 목록 조회 Controller, Service 단위 테스트 추가

--- src/test/java/com/t3t/bookstoreapi/book/controller/BookCategoryControllerUnitTest.java
+++ src/test/java/com/t3t/bookstoreapi/book/controller/BookCategoryControllerUnitTest.java
@@ -1,50 +1,70 @@
 package com.t3t.bookstoreapi.book.controller;
 
-import com.t3t.bookstoreapi.book.model.response.BookSearchResultResponse;
+import com.t3t.bookstoreapi.book.exception.BookNotFoundForIdException;
+import com.t3t.bookstoreapi.book.model.response.BookDetailResponse;
 import com.t3t.bookstoreapi.book.service.BookCategoryService;
+import com.t3t.bookstoreapi.category.exception.CategoryNotFoundException;
+import com.t3t.bookstoreapi.model.response.PageResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.http.MediaType;
+import org.springframework.data.domain.*;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.List;
+import java.util.Collections;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
+@RunWith(SpringRunner.class)
 @WebMvcTest(value = BookCategoryController.class)
+@ActiveProfiles("test")
 class BookCategoryControllerUnitTest {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @MockBean
     private BookCategoryService bookCategoryService;
 
-    @Test
     @DisplayName("특정 카테고리 ID에 해당하는 도서 목록 조회 테스트")
+    @Test
     void testGetBooksByCategoryId() throws Exception {
 
-//        int categoryId = 1;
-//
-//        BookSearchResultResponse bookResponse = BookSearchResultResponse.builder()
-//                .name("TestBookName").build();
-//
-//        Page<BookSearchResultResponse> pageResponse = new PageImpl<>(List.of(bookResponse));
-//
-//        when(bookCategoryService.findBooksByCategoryId(any(), any())).thenReturn(pageResponse);
-//
-//        mvc.perform(get("/category/{categoryId}/books", categoryId)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data.content[0].name", equalTo(bookResponse.getName())));
+        Integer categoryId = 1;
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bookId").descending());
+        PageResponse<BookDetailResponse> dummyResponse = PageResponse.<BookDetailResponse>builder()
+                .content(Collections.singletonList(BookDetailResponse.builder().id(1L).build()))
+                .pageNo(0)
+                .pageSize(10)
+                .totalElements(1)
+                .totalPages(1)
+                .last(true)
+                .build();
+        when(bookCategoryService.getBooksByCategoryId(categoryId, pageable)).thenReturn(dummyResponse);
+
+        mockMvc.perform(get("/category/{categoryId}/books", categoryId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists());
+    }
+
+    @DisplayName("존재하지 않는 카테고리 식별자로 도서 목록 조회시 예외 발생 테스트")
+    @Test
+    public void getBookDetailsById_thenReturns404() throws Exception {
+
+        Integer invalidCategoryId = -1;
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bookId").descending());
+
+        given(bookCategoryService.getBooksByCategoryId(invalidCategoryId, pageable)).willThrow(new CategoryNotFoundException());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/category/{categoryId}/books", invalidCategoryId))
+                .andExpect(status().isNotFound());
     }
 }
--- src/test/java/com/t3t/bookstoreapi/book/service/BookCategoryServiceUnitTest.java
+++ src/test/java/com/t3t/bookstoreapi/book/service/BookCategoryServiceUnitTest.java
@@ -1,18 +1,29 @@
 package com.t3t.bookstoreapi.book.service;
 
+
+import com.t3t.bookstoreapi.book.model.dto.ParticipantRoleRegistrationDtoByBookId;
+import com.t3t.bookstoreapi.book.model.response.BookDetailResponse;
 import com.t3t.bookstoreapi.book.repository.BookCategoryRepository;
 import com.t3t.bookstoreapi.book.repository.BookRepository;
+import com.t3t.bookstoreapi.category.exception.CategoryNotFoundException;
+import com.t3t.bookstoreapi.category.model.entity.Category;
 import com.t3t.bookstoreapi.category.repository.CategoryRepository;
+import com.t3t.bookstoreapi.model.response.PageResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BookCategoryServiceUnitTest {
@@ -27,84 +38,52 @@ class BookCategoryServiceUnitTest {
 
     @Test
     @DisplayName("특정 카테고리 ID에 해당하는 도서 목록 조회 테스트")
-    void testFindBooksByCategoryId() {
-//        Integer categoryId = 1;
-//
-//        Publisher publisher = Publisher.builder()
-//                .publisherName("TestPublisher")
-//                .publisherEmail("TestPublisher@example.com")
-//                .build();
-//
-//        Participant participant = Participant.builder()
-//                .participantId(1L)
-//                .participantName("ParticipantName")
-//                .build();
-//
-//        ParticipantRole participantRole = ParticipantRole.builder()
-//                .participantRoleId(1)
-//                .participantRoleNameKr("ParticipantRoleName")
-//                .build();
-//
-//        List<ParticipantRoleRegistration> authorList = List.of(
-//                ParticipantRoleRegistration.builder()
-//                        .participant(participant)
-//                        .participantRole(participantRole)
-//                        .build()
-//        );
-//
-//        Category parentCategory = Category.builder()
-//                .parentCategoryId(null)
-//                .categoryId(1)
-//                .categoryName("TestParentCategoryName")
-//                .build();
-//
-//        Category category = Category.builder()
-//                .parentCategoryId(1)
-//                .categoryId(2)
-//                .categoryName("TestCategoryName")
-//                .build();
-//
-//        Book book = Book.builder()
-//                .publisher(publisher)
-//                .bookName("어린왕자")
-//                .bookIndex("예시 목차")
-//                .bookDesc("어린왕자는 황소와의 대화, 별의 왕과의 대화, 장미꽃과의 대화 등을 통해 인생의 진리를 탐구하는 내용이 담겨있다.")
-//                .bookIsbn("9788966863307")
-//                .bookPrice(new BigDecimal("19.99"))
-//                .bookDiscount(new BigDecimal("0.1"))
-//                .bookPackage(TableStatus.TRUE)
-//                .bookPublished(LocalDate.of(1943, Month.APRIL, 6))
-//                .bookStock(100)
-//                .bookAverageScore(4.5f)
-//                .bookLikeCount(500)
-//                .authors(authorList)
-//                .bookThumbnail(BookThumbnail.builder().thumbnailImageUrl("TestImgUrl").build())
-//                .build();
-//
-//        BookCategory bookCategory = BookCategory.builder().book(book).category(category).build();
-//
-//        List<BookCategory> bookCategories = List.of(bookCategory);
-//
-//        Page<Book> page = new PageImpl<>(List.of(book));
-//
-//        given(categoryRepository.getChildCategoriesById(categoryId)).willReturn(List.of(category));
-//        given(bookCategoryRepository.findByCategoryCategoryIdIn(List.of(category.getCategoryId(), parentCategory.getCategoryId()))).willReturn(bookCategories);
-//        given(bookRepository.findByBookIdIn(anyList(), any(Pageable.class))).willReturn(page);
-//
-//        Pageable pageable = PageRequest.of(0, Integer.MAX_VALUE);
-//
-//        Page<BookSearchResultResponse> result = bookCategoryService.findBooksByCategoryId(categoryId, pageable);
-//
-//        assertEquals(book.getBookName(), result.getContent().get(0).getName());
+    void testGetBooksByCategoryId() {
+
+        // dummy data setting
+        Integer categoryId = 1;
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bookId").descending());
+
+        when(categoryRepository.existsById(categoryId)).thenReturn(true);
+        List<Category> childCategories = Arrays.asList(Category.builder().categoryId(2).build());
+        when(categoryRepository.getChildCategoriesById(categoryId)).thenReturn(childCategories);
+
+        List<Integer> targetCategoryIdList = Arrays.asList(2, 1);
+        Page<BookDetailResponse> bookDetailResponsePage = new PageImpl<>(Collections.emptyList());
+        when(bookCategoryRepository.getBooksByCategoryIds(targetCategoryIdList, pageable)).thenReturn(bookDetailResponsePage);
+
+        List<ParticipantRoleRegistrationDtoByBookId> participantDtoList = Collections.singletonList(ParticipantRoleRegistrationDtoByBookId.builder().build());
+        when(bookRepository.getBookParticipantDtoListByIdList(any())).thenReturn(participantDtoList);
+
+        PageResponse<BookDetailResponse> result = bookCategoryService.getBooksByCategoryId(categoryId, pageable);
+
+        assertNotNull(result);
+        assertEquals(0, result.getContent().size());
+        assertEquals(0, result.getPageNo());
+        assertEquals(0, result.getTotalElements());
+        assertEquals(1, result.getTotalPages());
+
+        verify(categoryRepository, times(1)).existsById(categoryId);
+        verify(categoryRepository, times(1)).getChildCategoriesById(categoryId);
+        verify(bookCategoryRepository, times(1)).getBooksByCategoryIds(targetCategoryIdList, pageable);
+        verify(bookRepository, times(1)).getBookParticipantDtoListByIdList(any());
     }
 
     @Test
     @DisplayName("특정 카테고리 ID에 해당하는 도서가 없는 경우 예외 발생 테스트")
     void testFindBooksByCategoryId_BookNotFoundForCategoryIdException() {
-//        Integer categoryId = 1;
-//
-//        when(categoryRepository.getChildCategoriesById(any())).thenReturn(new ArrayList<>());
-//
-//        assertThrows(BookNotFoundForCategoryIdException.class, () -> bookCategoryService.findBooksByCategoryId(categoryId, Pageable.unpaged()));
+        Integer categoryId = 1;
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bookId").descending());
+
+        when(categoryRepository.existsById(categoryId)).thenReturn(false);
+
+        assertThrows(CategoryNotFoundException.class, () -> {
+            bookCategoryService.getBooksByCategoryId(categoryId, pageable);
+        });
+
+        verify(categoryRepository, times(1)).existsById(categoryId);
+        verify(categoryRepository, never()).getChildCategoriesById(anyInt());
+        verify(bookCategoryRepository, never()).getBooksByCategoryIds(anyList(), any());
+        verify(bookRepository, never()).getBookParticipantDtoListByIdList(any());
     }
 }
