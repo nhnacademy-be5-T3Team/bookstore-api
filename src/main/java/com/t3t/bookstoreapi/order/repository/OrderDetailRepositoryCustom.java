19f7ffb62eeeb5b1550b5b7c1bc2e1224b73e0cf
woody35545 <woody35545@gmail.com>
2024-04-22 18:19:11 +0900

    feat: #104 - `OrderDetailRepositoryCustomImpl` -  주문 상세 식별자로 주문 상세 DTO 조회

--- src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustom.java
+++ src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustom.java
@@ -1,9 +1,18 @@
 package com.t3t.bookstoreapi.order.repository;
 
 import com.t3t.bookstoreapi.book.model.entity.Book;
+import com.t3t.bookstoreapi.order.model.dto.OrderDetailDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderDetailRepositoryCustom {
+    /**
+     * 주문 상세 식별자로 주문 상세 DTO 조회
+     *
+     * @author woody35545(구건모)
+     */
+    Optional<OrderDetailDto> getOrderDetailDtoById(long orderDetailId);
+
     List<Book> getSalesCountPerBook();
 }
--- src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustomImpl.java
+++ src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustomImpl.java
@@ -1,10 +1,13 @@
 package com.t3t.bookstoreapi.order.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.t3t.bookstoreapi.book.model.entity.Book;
+import com.t3t.bookstoreapi.order.model.dto.OrderDetailDto;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.t3t.bookstoreapi.order.model.entity.QOrderDetail.orderDetail;
 import static com.t3t.bookstoreapi.order.model.entity.QOrderStatus.orderStatus;
@@ -13,6 +16,31 @@ import static com.t3t.bookstoreapi.order.model.entity.QOrderStatus.orderStatus;
 public class OrderDetailRepositoryCustomImpl implements OrderDetailRepositoryCustom{
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    /**
+     * {@inheritDoc}
+     * @author woody35545(구건모)
+     */
+    @Override
+    public Optional<OrderDetailDto> getOrderDetailDtoById(long orderDetailId) {
+        return Optional.ofNullable(jpaQueryFactory.select(
+                        Projections.bean(OrderDetailDto.class,
+                                orderDetail.id.as("id"),
+                                orderDetail.quantity.as("quantity"),
+                                orderDetail.createdAt.as("createdAt"),
+                                orderDetail.order.id.as("orderId"),
+                                orderDetail.book.bookId.as("bookId"),
+                                orderDetail.book.bookName.as("bookName"),
+                                orderDetail.book.publisher.publisherName.as("bookPublisherName"),
+                                orderDetail.book.bookPrice.as("bookPrice"),
+                                orderDetail.book.bookDiscount.as("bookDiscount"),
+                                orderDetail.packaging.name.as("packagingName"),
+                                orderDetail.packaging.price.as("packagingPrice"),
+                                orderDetail.orderStatus.name.as("orderStatusName")))
+                .from(orderDetail)
+                .where(orderDetail.id.eq(orderDetailId))
+                .fetchOne());
+    }
 
     @Override
     public List<Book> getSalesCountPerBook() {
