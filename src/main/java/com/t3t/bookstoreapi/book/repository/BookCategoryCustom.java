bca63758b9ef7626630a1d4d5c4bb0d00376a469
yujinKim <yujinkimvv@gmail.com>
2024-04-22 15:39:09 +0900

    fix : #98 카테고리 id 리스트에 해당하는 도서 목록 조회 query dsl 적용

--- src/main/java/com/t3t/bookstoreapi/book/repository/BookCategoryCustom.java
+++ src/main/java/com/t3t/bookstoreapi/book/repository/BookCategoryCustom.java
@@ -1,2 +1,20 @@
-package com.t3t.bookstoreapi.book.repository;public interface BookCategoryCustom {
+package com.t3t.bookstoreapi.book.repository;
+
+import com.t3t.bookstoreapi.book.model.response.BookDetailResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface BookCategoryCustom {
+    /**
+     * 주어진 카테고리 ID 목록을 기준으로 해당 카테고리에 속하는 도서 목록을 가져옴
+     * 페이지네이션 기능을 포함하며, 각 도서에 대한 기본 정보만을 포함한 BookDetailResponse 객체 목록을 반환
+     *
+     * @param categoryIdList 카테고리 ID 목록
+     * @param pageable       페이지 정보
+     * @return 주어진 카테고리에 속하는 도서 목록의 페이지
+     * @author Yujin-nKim(김유진)
+     */
+    Page<BookDetailResponse> getBooksByCategoryIds(List<Integer> categoryIdList, Pageable pageable);
 }
