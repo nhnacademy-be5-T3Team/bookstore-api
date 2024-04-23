25dec538e2aed61c7547b53647b25e81118bc23d
woody35545 <woody35545@gmail.com>
2024-04-23 12:56:05 +0900

    test: #104  `OrderDetailServiceUnitTest` - 주문 식별자로 주문 상세 리스트 조회 테스트

--- src/test/java/com/t3t/bookstoreapi/order/servcie/OrderDetailServiceUnitTest.java
+++ src/test/java/com/t3t/bookstoreapi/order/servcie/OrderDetailServiceUnitTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -70,5 +71,54 @@ public class OrderDetailServiceUnitTest {
         assertEquals(testOrderDetailDto.getPackagingName(), resultOrderDetailDto.getPackagingName());
         assertEquals(testOrderDetailDto.getPackagingPrice(), resultOrderDetailDto.getPackagingPrice());
         assertEquals(testOrderDetailDto.getOrderStatusName(), resultOrderDetailDto.getOrderStatusName());
+    }
+
+
+    /**
+     * 주문 상세 조회 - 주문 식별자로 주문 상세 리스트 조회
+     * @see OrderDetailService#getOrderDetailDtoListByOrderId(long)
+     * @author woody35545(구건모)
+     */
+    @Test
+    @DisplayName("주문 상세 조회 - 주문 식별자로 주문 상세 리스트 조회")
+    void getOrderDetailDtoListByOrderIdTest() {
+        // given
+        long orderId = 0L;
+        OrderDetailDto testOrderDetailDto = OrderDetailDto.builder()
+                .id(0L)
+                .quantity(1L)
+                .createdAt(LocalDateTime.now())
+                .orderId(0L)
+                .bookId(0L)
+                .bookName("testBookName")
+                .bookPublisherName("testPublisherName")
+                .bookDiscount(new BigDecimal("0"))
+                .bookPrice(new BigDecimal("10000"))
+                .packagingName("testPackagingName")
+                .packagingPrice(new BigDecimal("1000"))
+                .orderStatusName("testOrderStatusName")
+                .build();
+
+        when(orderDetailRepository.getOrderDetailDtoListByOrderId(orderId)).thenReturn(
+                List.of(testOrderDetailDto));
+
+        // when
+        List<OrderDetailDto> resultOrderDetailDtoList = orderDetailService.getOrderDetailDtoListByOrderId(orderId);
+
+        // then
+        assertEquals(1, resultOrderDetailDtoList.size());
+        assertEquals(testOrderDetailDto, resultOrderDetailDtoList.get(0));
+        assertEquals(testOrderDetailDto.getId(), resultOrderDetailDtoList.get(0).getId());
+        assertEquals(testOrderDetailDto.getQuantity(), resultOrderDetailDtoList.get(0).getQuantity());
+        assertEquals(testOrderDetailDto.getCreatedAt(), resultOrderDetailDtoList.get(0).getCreatedAt());
+        assertEquals(testOrderDetailDto.getOrderId(), resultOrderDetailDtoList.get(0).getOrderId());
+        assertEquals(testOrderDetailDto.getBookId(), resultOrderDetailDtoList.get(0).getBookId());
+        assertEquals(testOrderDetailDto.getBookName(), resultOrderDetailDtoList.get(0).getBookName());
+        assertEquals(testOrderDetailDto.getBookPublisherName(), resultOrderDetailDtoList.get(0).getBookPublisherName());
+        assertEquals(testOrderDetailDto.getBookPrice(), resultOrderDetailDtoList.get(0).getBookPrice());
+        assertEquals(testOrderDetailDto.getBookDiscount(), resultOrderDetailDtoList.get(0).getBookDiscount());
+        assertEquals(testOrderDetailDto.getPackagingName(), resultOrderDetailDtoList.get(0).getPackagingName());
+        assertEquals(testOrderDetailDto.getPackagingPrice(), resultOrderDetailDtoList.get(0).getPackagingPrice());
+        assertEquals(testOrderDetailDto.getOrderStatusName(), resultOrderDetailDtoList.get(0).getOrderStatusName());
     }
 }
