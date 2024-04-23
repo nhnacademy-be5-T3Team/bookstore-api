d5de43e1b5876d549e68d187177027c48da9ee8a
yujinKim <yujinkimvv@gmail.com>
2024-04-23 23:17:14 +0900

    refactor : 불필요한 Import 문 제거

--- src/main/java/com/t3t/bookstoreapi/category/controller/CategoryController.java
+++ src/main/java/com/t3t/bookstoreapi/category/controller/CategoryController.java
@@ -1,19 +1,15 @@
 package com.t3t.bookstoreapi.category.controller;
 
-import com.t3t.bookstoreapi.category.model.request.CategoryListRequest;
 import com.t3t.bookstoreapi.category.model.response.CategoryTreeResponse;
 import com.t3t.bookstoreapi.category.service.CategoryService;
 import com.t3t.bookstoreapi.model.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Validated
