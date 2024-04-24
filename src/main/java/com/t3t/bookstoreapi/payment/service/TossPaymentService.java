d9fa06cdfb6e40392dbd4dd537b1d21a08d9e148
woody35545 <woody35545@gmail.com>
2024-04-24 23:03:37 +0900

    modify: #92 `TossPaymentService` - 예외 변경

--- src/main/java/com/t3t/bookstoreapi/payment/service/TossPaymentService.java
+++ src/main/java/com/t3t/bookstoreapi/payment/service/TossPaymentService.java
@@ -7,6 +7,7 @@ import com.t3t.bookstoreapi.order.repository.OrderRepository;
 import com.t3t.bookstoreapi.payment.client.TossPaymentApiClient;
 import com.t3t.bookstoreapi.payment.constant.PaymentProviderType;
 import com.t3t.bookstoreapi.payment.constant.TossPaymentStatus;
+import com.t3t.bookstoreapi.payment.exception.PaymentProviderNotFoundForNameException;
 import com.t3t.bookstoreapi.payment.exception.UnsupportedPaymentProviderTypeException;
 import com.t3t.bookstoreapi.payment.model.dto.PaymentDto;
 import com.t3t.bookstoreapi.payment.model.entity.Payment;
@@ -40,7 +41,7 @@ public class TossPaymentService implements ProviderPaymentService {
     public PaymentDto createPayment(PaymentCreationRequest request) {
 
         PaymentProvider paymentProvider = paymentProviderRepository.findByName(PaymentProviderType.TOSS.toString())
-                .orElseThrow(() -> new UnsupportedPaymentProviderTypeException(PaymentProviderType.TOSS.toString()));
+                .orElseThrow(() -> new PaymentProviderNotFoundForNameException(PaymentProviderType.TOSS.toString()));
 
         Order order = orderRepository.findById(request.getOrderId())
                 .orElseThrow(() -> new OrderNotFoundForIdException(request.getOrderId()));
