5bd2ac24d2d50242b396ce2c547b5c5cce6df027
yujinKim <yujinkimvv@gmail.com>
2024-04-25 23:58:39 +0900

    #118 RedisConfig 수정 - restTemplate 방식으로 redis 서버와 연결

--- src/main/java/com/t3t/bookstoreapi/config/RedisConfig.java
+++ src/main/java/com/t3t/bookstoreapi/config/RedisConfig.java
@@ -5,9 +5,15 @@ import com.t3t.bookstoreapi.property.RedisProperties;
 import com.t3t.bookstoreapi.property.SecretKeyProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Bean
     public RedisProperties redisProperties(SecretKeyManagerService secretKeyManagerService,
                                            SecretKeyProperties secretKeyProperties) {
@@ -15,6 +21,26 @@ public class RedisConfig {
                 .redisServerIpAddress(secretKeyManagerService.getSecretValue(secretKeyProperties.getRedisServerIpAddressKeyId()))
                 .redisServerPort(secretKeyManagerService.getSecretValue(secretKeyProperties.getRedisServerPortKeyId()))
                 .redisServerPassword(secretKeyManagerService.getSecretValue(secretKeyProperties.getRedisServerPasswordKeyId()))
+                .redisDatabase(21)
                 .build();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisProperties.getRedisServerIpAddress());
+        redisStandaloneConfiguration.setPort(Integer.parseInt(redisProperties.getRedisServerPort()));
+        redisStandaloneConfiguration.setPassword(redisProperties.getRedisServerPassword());
+        redisStandaloneConfiguration.setDatabase(21);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisProperties redisProperties) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory(redisProperties));
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
     }
 }
--- src/main/java/com/t3t/bookstoreapi/property/RedisProperties.java
+++ src/main/java/com/t3t/bookstoreapi/property/RedisProperties.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Builder
 public class RedisProperties {
     private String redisServerIpAddress;
-    private String redisServerPort;
     private String redisServerPassword;
+    private String redisServerPort;
+    private Integer redisDatabase;
 }
