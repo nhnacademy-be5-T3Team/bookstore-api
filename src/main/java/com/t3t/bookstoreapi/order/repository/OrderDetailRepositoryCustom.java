bba550418a1dbd754b90743668f13970c68c104c
woody35545 <woody35545@gmail.com>
2024-04-25 13:11:09 +0900

    feat: #111 `OrderDetailRepositoryCustom` - 주문 식별자로 주문 상세 엔티티 리스트 조회하는 QueryDSL 메서드

--- src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustom.java
+++ src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustom.java
@@ -1,7 +1,7 @@
 package com.t3t.bookstoreapi.order.repository;
 
-import com.t3t.bookstoreapi.book.model.entity.Book;
 import com.t3t.bookstoreapi.order.model.dto.OrderDetailDto;
+import com.t3t.bookstoreapi.order.model.entity.OrderDetail;
 import com.t3t.bookstoreapi.recommendation.model.response.BookInfoBriefResponse;
 
 import java.util.List;
@@ -14,10 +14,20 @@ public interface OrderDetailRepositoryCustom {
      * @author woody35545(구건모)
      */
     Optional<OrderDetailDto> getOrderDetailDtoById(long orderDetailId);
+
     /**
      * 주문 식별자로 주문 상세 DTO 리스트 조회
+     *
      * @author woody35545(구건모)
      */
     List<OrderDetailDto> getOrderDetailDtoListByOrderId(long orderId);
+
+    /**
+     * 주문 식별자로 주문 상세 엔티티 리스트 조회
+     *
+     * @author woody35545(구건모)
+     */
+    List<OrderDetail> getOrderDetailListByOrderId(long orderId);
+
     List<BookInfoBriefResponse> getSalesCountPerBook(int maxCount);
 }
--- src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustomImpl.java
+++ src/main/java/com/t3t/bookstoreapi/order/repository/OrderDetailRepositoryCustomImpl.java
@@ -4,8 +4,10 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.QBean;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.t3t.bookstoreapi.book.model.entity.Book;
+import com.t3t.bookstoreapi.order.constant.OrderStatusType;
 import com.t3t.bookstoreapi.order.model.dto.OrderDetailDto;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.t3t.bookstoreapi.order.model.entity.OrderDetail;
 import com.t3t.bookstoreapi.recommendation.model.response.BookInfoBriefResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -14,8 +16,10 @@ import java.util.Optional;
 
 import static com.t3t.bookstoreapi.book.model.entity.QBook.book;
 import static com.t3t.bookstoreapi.book.model.entity.QBookThumbnail.bookThumbnail;
+import static com.t3t.bookstoreapi.order.model.entity.QOrder.order;
 import static com.t3t.bookstoreapi.order.model.entity.QOrderDetail.orderDetail;
 import static com.t3t.bookstoreapi.order.model.entity.QOrderStatus.orderStatus;
+import static com.t3t.bookstoreapi.order.model.entity.QPackaging.packaging;
 
 @RequiredArgsConstructor
 public class OrderDetailRepositoryCustomImpl implements OrderDetailRepositoryCustom {
@@ -33,6 +37,7 @@ public class OrderDetailRepositoryCustomImpl implements OrderDetailRepositoryCus
             orderDetail.book.publisher.publisherName.as("bookPublisherName"),
             orderDetail.book.bookPrice.as("bookPrice"),
             orderDetail.book.bookDiscount.as("bookDiscount"),
+            orderDetail.packaging.id.as("packagingId"),
             orderDetail.packaging.name.as("packagingName"),
             orderDetail.packaging.price.as("packagingPrice"),
             orderDetail.orderStatus.name.as("orderStatusName")
@@ -63,6 +68,23 @@ public class OrderDetailRepositoryCustomImpl implements OrderDetailRepositoryCus
                 .fetch();
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @author woody35545(구건모)
+     */
+
+    @Override
+    public List<OrderDetail> getOrderDetailListByOrderId(long orderId) {
+        return jpaQueryFactory.selectFrom(orderDetail)
+                .join(orderDetail.order).fetchJoin()
+                .join(orderDetail.book).fetchJoin()
+                .leftJoin(orderDetail.packaging).fetchJoin()
+                .join(orderDetail.orderStatus).fetchJoin()
+                .where(orderDetail.order.id.eq(orderId))
+                .fetch();
+    }
+
     @Override
     public List<BookInfoBriefResponse> getSalesCountPerBook(int maxCount) {
 
@@ -70,13 +92,15 @@ public class OrderDetailRepositoryCustomImpl implements OrderDetailRepositoryCus
                 .select(Projections.fields(BookInfoBriefResponse.class,
                         book.bookId.as("id"),
                         book.bookName.as("name"),
-                        bookThumbnail.thumbnailImageUrl.as("thumbnailImageUrl")
-                ))
+                        bookThumbnail.thumbnailImageUrl.as("thumbnailImageUrl")))
                 .from(orderDetail)
                 .join(orderDetail.orderStatus, orderStatus)
                 .leftJoin(orderDetail.book, book)
                 .leftJoin(bookThumbnail).on(bookThumbnail.book.bookId.eq(book.bookId))
-                .where(orderStatus.name.in("PENDING", "DELIVERING", "DELIVERED"))
+                .where(orderStatus.name.in(
+                        OrderStatusType.PENDING.toString(),
+                        OrderStatusType.DELIVERING.toString(),
+                        OrderStatusType.DELIVERED.toString()))
                 .groupBy(book.bookId, bookThumbnail.thumbnailImageUrl)
                 .orderBy(orderDetail.quantity.sum().desc())
                 .limit(maxCount)
