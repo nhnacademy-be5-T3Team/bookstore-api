e52bb5254fe210d69aed64878847447d4c6dd623
woody35545 <woody35545@gmail.com>
2024-04-26 03:12:16 +0900

    feat: #119 `OrderService` - 특정 주문에 속한 모든 주문 상세 항목들의 주문 상태 변경 기능

--- src/main/java/com/t3t/bookstoreapi/order/service/OrderService.java
+++ src/main/java/com/t3t/bookstoreapi/order/service/OrderService.java
@@ -1,7 +1,6 @@
 package com.t3t.bookstoreapi.order.service;
 
 import com.t3t.bookstoreapi.member.exception.MemberNotFoundForIdException;
-import com.t3t.bookstoreapi.member.model.entity.Member;
 import com.t3t.bookstoreapi.member.repository.MemberRepository;
 import com.t3t.bookstoreapi.order.constant.OrderStatusType;
 import com.t3t.bookstoreapi.order.exception.DeliveryNotFoundForIdException;
@@ -10,7 +9,7 @@ import com.t3t.bookstoreapi.order.exception.OrderStatusNotFoundForNameException;
 import com.t3t.bookstoreapi.order.model.dto.OrderDto;
 import com.t3t.bookstoreapi.order.model.entity.Order;
 import com.t3t.bookstoreapi.order.model.entity.OrderDetail;
-import com.t3t.bookstoreapi.order.model.request.GuestOrderCreationRequest;
+import com.t3t.bookstoreapi.order.model.entity.OrderStatus;
 import com.t3t.bookstoreapi.order.model.request.MemberOrderCreationRequest;
 import com.t3t.bookstoreapi.order.repository.DeliveryRepository;
 import com.t3t.bookstoreapi.order.repository.OrderDetailRepository;
@@ -20,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -36,6 +34,7 @@ public class OrderService {
 
     /**
      * 회원 주문 생성
+     *
      * @param request 회원 주문 생성 요청 객체
      * @return 생성된 주문 DTO
      * @autor woody35545(구건모)
@@ -48,6 +47,26 @@ public class OrderService {
                         .orElseThrow(() -> new DeliveryNotFoundForIdException(request.getDeliveryId())))
                 .createdAt(LocalDateTime.now())
                 .build()));
+    }
+
+    /**
+     * 주문 상태를 변경한다.<br>
+     * 특정 주문에 속한 모든 주문 상세 항목들의 주문 상태를 변경한다.
+     *
+     * @param orderId 주문 상태를 변경하고자 하는 주문 식별자
+     * @author woody35545(구건모)
+     */
+    public void modifyOrderStatusByOrderId(Long orderId, OrderStatusType orderStatusType) {
+
+        if (!orderRepository.existsById(orderId)) {
+            throw new OrderNotFoundForIdException(orderId);
+        }
+
+        OrderStatus orderStatus = orderStatusRepository.findByName(orderStatusType.toString())
+                .orElseThrow(() -> new OrderStatusNotFoundForNameException(orderStatusType.toString()));
+
+        orderDetailRepository.getOrderDetailListByOrderId(orderId)
+                .forEach(orderDetail -> orderDetail.setOrderStatus(orderStatus));
     }
 
 }
