bbad2b6b8843aeea6a88f9844269cd9171a5137f
woody35545 <woody35545@gmail.com>
2024-04-29 05:23:44 +0900

    feat: #121 `OrderController` - 주문 승인 API

--- src/main/java/com/t3t/bookstoreapi/order/controller/OrderController.java
+++ src/main/java/com/t3t/bookstoreapi/order/controller/OrderController.java
@@ -2,13 +2,12 @@ package com.t3t.bookstoreapi.order.controller;
 
 import com.t3t.bookstoreapi.model.response.BaseResponse;
 import com.t3t.bookstoreapi.order.model.dto.OrderDetailDto;
+import com.t3t.bookstoreapi.order.model.request.OrderConfirmRequest;
 import com.t3t.bookstoreapi.order.service.OrderDetailService;
+import com.t3t.bookstoreapi.order.service.OrderServiceFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +15,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderDetailService orderDetailService;
+    private final OrderServiceFacade orderServiceFacade;
 
     /**
      * 주문 내에 속해있는 주문 상세 리스트 조회
+     *
      * @param orderId 조회하려는 주문 식별자
      * @author woody35545(구건모)
      */
@@ -27,5 +28,18 @@ public class OrderController {
     public BaseResponse<List<OrderDetailDto>> getOrderDetailDtoListByOrderId(@PathVariable("orderId") long orderId) {
 
         return new BaseResponse<List<OrderDetailDto>>().data(orderDetailService.getOrderDetailDtoListByOrderId(orderId));
+    }
+
+    /**
+     * 주문 승인
+     *
+     * @param orderConfirmRequest 주문 승인 요청 정보
+     * @author woody35545(구건모)
+     */
+    @PostMapping("/orders/confirm")
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponse<Void> confirmOrder(OrderConfirmRequest orderConfirmRequest) {
+        orderServiceFacade.confirmOrder(orderConfirmRequest);
+        return new BaseResponse<Void>().message("주문 승인이 완료되었습니다.");
     }
 }
