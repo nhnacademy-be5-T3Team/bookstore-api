399cd653f91a9f67e19c0e3f63bccb5e607bbf24
parkjonggyeong18 <gd122572@gmail.com>
2024-04-30 18:50:55 +0900

    feat: 다양한 형태소 분석기 사용

--- src/main/java/com/t3t/bookstoreapi/elastic/repository/ElasticRepositoryImpl.java
+++ src/main/java/com/t3t/bookstoreapi/elastic/repository/ElasticRepositoryImpl.java
@@ -2,11 +2,13 @@ package com.t3t.bookstoreapi.elastic.repository;
 
 import com.t3t.bookstoreapi.elastic.model.dto.ElasticDocument;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
@@ -16,100 +18,83 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilde
 public class ElasticRepositoryImpl implements ElasticRepositoryCustom {
     private final ElasticsearchOperations elasticsearchOperations;
     /**
-     *
-     * @param query text 검색어
+     * @param query    text 검색어
      * @param pageable 페이지 요청 정보
      * @return 모든 검색유형으로 찾은 도서 목록
-     *  @author parkjonggyeong18(박종경)
+     * @author parkjonggyeong18(박종경)
      */
     @Override
     public SearchHits<ElasticDocument> findByAll(String query, Pageable pageable) {
-        // 정확한 일치 쿼리
-        QueryBuilder exactMatchQuery = QueryBuilders.multiMatchQuery(query)
-                .field("book_name")
-                .field("publisher_name")
-                .field("participant_name")
-                .type(MultiMatchQueryBuilder.Type.PHRASE);
-        // 정확한 일치 쿼리 실행
-        SearchHits<ElasticDocument> exactMatches = elasticPageable(exactMatchQuery, pageable);
-        if (exactMatches.hasSearchHits()) {
-            return exactMatches;
-        }
         // 키워드 검색 쿼리
         QueryBuilder keywordMatchQuery = QueryBuilders.multiMatchQuery(query)
-                .field("book_name")
-                .field("publisher_name")
-                .field("participant_name");
+                .field("book_name", 100) //형태소 검색
+                .field("book_name.ngram", 90)//띄어쓰기, 숫자등 검색
+                .field("book_name.jaso", 95)//자음, 오타 검색
+
+                .field("publisher_name", 25)
+                .field("publisher_name.ngram", 15)
+                .field("publisher_name.jaso", 20)
+
+                .field("participant_name", 55)
+                .field("participant_name.ngram", 45)
+                .field("participant_name.jaso", 50);
+
         // 키워드 검색 쿼리 실행
         return elasticPageable(keywordMatchQuery, pageable);
     }
     /**
-     *
-     * @param query text 검색어
+     * @param query    text 검색어
      * @param pageable 페이지 요청 정보
      * @return 도서 제목으로 찾은 도서 목록
-     *  @author parkjonggyeong18(박종경)
+     * @author parkjonggyeong18(박종경)
      */
     @Override
     public SearchHits<ElasticDocument> findByBookName(String query, Pageable pageable) {
-        QueryBuilder exactMatchQuery = QueryBuilders.matchPhraseQuery("book_name", query);
-        // 정확한 일치 쿼리 실행
-        SearchHits<ElasticDocument> exactMatches = elasticPageable(exactMatchQuery, pageable);
-        if (exactMatches.hasSearchHits()) {
-            return exactMatches;
-        }
+
         // 키워드 검색 쿼리
-        QueryBuilder keywordMatchQuery = QueryBuilders.matchQuery("book_name", query);
+        QueryBuilder keywordMatchQuery = QueryBuilders.multiMatchQuery(query)
+                .field("book_name")
+                .field("book_name.ngram")
+                .field("book_name.jaso");
         // 키워드 검색 쿼리 실행
         return elasticPageable(keywordMatchQuery, pageable);
     }
-
     /**
-     *
-     * @param query text 검색어
+     * @param query    text 검색어
      * @param pageable 페이지 요청 정보
      * @return 출판사 명으로 찾은 도서 목록
-     *  @author parkjonggyeong18(박종경)
+     * @author parkjonggyeong18(박종경)
      */
     @Override
     public SearchHits<ElasticDocument> findByPublisher(String query, Pageable pageable) {
-        QueryBuilder exactMatchQuery = QueryBuilders.matchPhraseQuery("publisher_name", query);
-        // 정확한 일치 쿼리 실행
-        SearchHits<ElasticDocument> exactMatches = elasticPageable(exactMatchQuery, pageable);
-        if (exactMatches.hasSearchHits()) {
-            return exactMatches;
-        }
+
         // 키워드 검색 쿼리
-        QueryBuilder keywordMatchQuery = QueryBuilders.matchQuery("publisher_name", query);
+        QueryBuilder keywordMatchQuery = QueryBuilders.multiMatchQuery(query)
+                .field("publisher_name")
+                .field("publisher_name.ngram")
+                .field("publisher_name.jaso");
         // 키워드 검색 쿼리 실행
         return elasticPageable(keywordMatchQuery, pageable);
     }
-
     /**
-     *
-     * @param query text 검색어
+     * @param query    text 검색어
      * @param pageable 페이지 요청 정보
      * @return 참여자 명으로 찾은 도서 목록
-     *  @author parkjonggyeong18(박종경)
+     * @author parkjonggyeong18(박종경)
      */
     @Override
     public SearchHits<ElasticDocument> findByAuthorName(String query, Pageable pageable) {
-        QueryBuilder exactMatchQuery = QueryBuilders.matchPhraseQuery("participant_name", query);
-        // 정확한 일치 쿼리 실행
-        SearchHits<ElasticDocument> exactMatches = elasticPageable(exactMatchQuery, pageable);
-        if (exactMatches.hasSearchHits()) {
-            return exactMatches;
-        }
         // 키워드 검색 쿼리
-        QueryBuilder keywordMatchQuery = QueryBuilders.matchQuery("participant_name", query);
+        QueryBuilder keywordMatchQuery = QueryBuilders.multiMatchQuery(query)
+                .field("participant_name")
+                .field("participant_name.ngram")
+                .field("participant_name.jaso");
         // 키워드 검색 쿼리 실행
         return elasticPageable(keywordMatchQuery, pageable);
     }
-
     /**
-     *
      * @param queryBuilder 키워드를 통해 검색된 도서 목록
-     * @param pageable 페이지 요청 정보
+     * @param pageable     페이지 요청 정보
      * @return elasticsearch의 정렬된 도서 목록
      */
     public SearchHits<ElasticDocument> elasticPageable(QueryBuilder queryBuilder, Pageable pageable) {
