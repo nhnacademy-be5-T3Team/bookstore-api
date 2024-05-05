4d8e437e97bb44db1063ce7cc8cc701a19e1bed8
woody35545 <woody35545@gmail.com>
2024-05-05 14:10:18 +0900

    feat: #138 `MemberRepositoryCustomImpl`- 회원 식별자로 회원 정보 조회

---
+++ src/main/java/com/t3t/bookstoreapi/member/repository/MemberRepositoryCustomImpl.java
@@ -0,0 +1,44 @@
+package com.t3t.bookstoreapi.member.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.t3t.bookstoreapi.member.model.response.MemberInfoResponse;
+import lombok.RequiredArgsConstructor;
+
+import static com.t3t.bookstoreapi.member.model.entity.QAccount.account;
+import static com.t3t.bookstoreapi.member.model.entity.QMember.member;
+
+@RequiredArgsConstructor
+public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    /**
+     * 회원 식별자로 회원의 정보를 조회한다.
+     *
+     * @param memberId 조회하려는 회원의 식별자
+     * @author woody35545
+     * @see MemberInfoResponse
+     */
+    @Override
+    public MemberInfoResponse getMemberInfoResponseByMemberId(long memberId) {
+        return queryFactory.select(Projections.bean(
+                        MemberInfoResponse.class,
+                        member.id.as("memberId"),
+                        member.name.as("name"),
+                        member.phone.as("phone"),
+                        member.email.as("email"),
+                        member.birthDate.as("birthDate"),
+                        member.latestLogin.as("latestLogin"),
+                        member.point.as("point"),
+                        member.grade.gradeId.as("gradeId"),
+                        member.grade.name.as("gradeName"),
+                        member.status.as("status"),
+                        member.role.as("role"),
+                        account.id.as("accountId")))
+                .from(member)
+                .join(account).on(member.eq(account.member))
+                .where(member.id.eq(memberId))
+                .fetchOne();
+    }
+}
