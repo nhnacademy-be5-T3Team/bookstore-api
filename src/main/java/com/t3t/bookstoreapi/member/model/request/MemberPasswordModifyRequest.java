249043f384ff4d8ea2b8dad631fe0d7cf4cde07d
woody35545 <woody35545@gmail.com>
2024-05-05 15:38:20 +0900

    update: #139 `MemberPasswordModifyRequest` - validation 추가

--- src/main/java/com/t3t/bookstoreapi/member/model/request/MemberPasswordModifyRequest.java
+++ src/main/java/com/t3t/bookstoreapi/member/model/request/MemberPasswordModifyRequest.java
@@ -2,6 +2,8 @@ package com.t3t.bookstoreapi.member.model.request;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * 회원 비밀번호 수정 요청 객체
  *
@@ -12,6 +14,8 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberPasswordModifyRequest {
+    @NotBlank(message = "현재 비밀번호가 누락되었습니다.")
     private String currentPassword;
+    @NotBlank(message = "새로운 비밀번호가 누락되었습니다.")
     private String newPassword;
 }
