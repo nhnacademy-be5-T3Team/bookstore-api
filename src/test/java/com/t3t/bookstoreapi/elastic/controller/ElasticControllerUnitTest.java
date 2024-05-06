f81c8f4a094fbb9d281d8078c15075a923e7cefd
parkjonggyeong18 <gd122572@gmail.com>
2024-05-06 10:52:54 +0900

    test: #101 카테고리 검색 기능 test 구현

--- src/test/java/com/t3t/bookstoreapi/elastic/controller/ElasticControllerUnitTest.java
+++ src/test/java/com/t3t/bookstoreapi/elastic/controller/ElasticControllerUnitTest.java
@@ -2,8 +2,8 @@ package com.t3t.bookstoreapi.elastic.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.t3t.bookstoreapi.elastic.model.response.ElasticResponse;
+import com.t3t.bookstoreapi.elastic.service.ElasticCategoryService;
 import com.t3t.bookstoreapi.elastic.service.ElasticService;
-import com.t3t.bookstoreapi.model.response.BaseResponse;
 import com.t3t.bookstoreapi.model.response.PageResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,7 +23,8 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ElasticController.class)
 class ElasticControllerUnitTest {
@@ -35,6 +33,8 @@ class ElasticControllerUnitTest {
 
     @MockBean
     private ElasticService elasticService;
+    @MockBean
+    private ElasticCategoryService elasticCategoryService;
 
     private ObjectMapper objectMapper;
 
@@ -73,29 +73,30 @@ class ElasticControllerUnitTest {
     }
 
 
-@Test
-@DisplayName("엘라스틱 서치 목록에 없는 도서 검색 테스트")
-void getSearchPage_NoResults() throws Exception {
-    PageResponse<ElasticResponse> pageResponse = new PageResponse<>();
-    pageResponse.setContent(Collections.emptyList());
-    pageResponse.setPageNo(0);
-    pageResponse.setPageSize(10);
-    pageResponse.setTotalElements(0);
-    pageResponse.setTotalPages(0);
-    pageResponse.setLast(true);
+    @Test
+    @DisplayName("엘라스틱 서치 목록에 없는 도서 검색 테스트")
+    void getSearchPage_NoResults() throws Exception {
+        PageResponse<ElasticResponse> pageResponse = new PageResponse<>();
+        pageResponse.setContent(Collections.emptyList());
+        pageResponse.setPageNo(0);
+        pageResponse.setPageSize(10);
+        pageResponse.setTotalElements(0);
+        pageResponse.setTotalPages(0);
+        pageResponse.setLast(true);
 
-    when(elasticService.search(anyString(), anyString(), any(Pageable.class))).thenReturn(pageResponse);
+        when(elasticService.search(anyString(), anyString(), any(Pageable.class))).thenReturn(pageResponse);
 
-    mockMvc.perform(MockMvcRequestBuilders.get("/search")
-                    .param("query", "없는책")
-                    .param("searchType", "book_name")
-                    .param("pageNo", "0")
-                    .param("pageSize", "10")
-                    .param("sortBy", "_score")
-                    .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isNoContent())
-            .andExpect(jsonPath("$.message").value("검색한 도서가 없습니다."));
-}
+        mockMvc.perform(MockMvcRequestBuilders.get("/search")
+                        .param("query", "없는책")
+                        .param("searchType", "book_name")
+                        .param("pageNo", "0")
+                        .param("pageSize", "10")
+                        .param("sortBy", "_score")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$.message").value("검색한 도서가 없습니다."));
+    }
+
     @Test
     @DisplayName("검색어가 없는 예외 테스트")
     void getSearchPage_BadRequest() throws Exception {
@@ -103,5 +104,60 @@ void getSearchPage_NoResults() throws Exception {
                         .param("query", "")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("elasticSearch 기능 test - with categoryId")
+    void getSearchPage_ReturnsResults_WithCategoryId() throws Exception {
+        ElasticResponse elasticResponse = new ElasticResponse();
+        elasticResponse.setName("테스트 책");
+        elasticResponse.setBookId(new BigDecimal("123"));
+
+        List<ElasticResponse> content = Collections.singletonList(elasticResponse);
+        PageResponse<ElasticResponse> pageResponse = new PageResponse<>();
+        pageResponse.setContent(content);
+        pageResponse.setPageNo(0);
+        pageResponse.setPageSize(10);
+        pageResponse.setTotalElements(1);
+        pageResponse.setTotalPages(1);
+        pageResponse.setLast(true);
+
+        when(elasticCategoryService.search(anyString(), anyString(), any(BigDecimal.class), any(Pageable.class))).thenReturn(pageResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/search")
+                        .param("query", "책")
+                        .param("searchType", "book_name")
+                        .param("categoryId", "123")
+                        .param("pageNo", "0")
+                        .param("pageSize", "10")
+                        .param("sortBy", "_score")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.content[0].name").value("테스트 책"));
+    }
+
+    @Test
+    @DisplayName("엘라스틱 서치 목록에 없는 도서 검색 테스트 - with categoryId")
+    void getSearchPage_NoResults_WithCategoryId() throws Exception {
+        PageResponse<ElasticResponse> pageResponse = new PageResponse<>();
+        pageResponse.setContent(Collections.emptyList());
+        pageResponse.setPageNo(0);
+        pageResponse.setPageSize(10);
+        pageResponse.setTotalElements(0);
+        pageResponse.setTotalPages(0);
+        pageResponse.setLast(true);
+
+        when(elasticCategoryService.search(anyString(), anyString(), any(BigDecimal.class), any(Pageable.class))).thenReturn(pageResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/search")
+                        .param("query", "없는책")
+                        .param("searchType", "book_name")
+                        .param("categoryId", "123")
+                        .param("pageNo", "0")
+                        .param("pageSize", "10")
+                        .param("sortBy", "_score")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$.message").value("검색한 도서가 없습니다."));
     }
 }
--- src/test/java/com/t3t/bookstoreapi/elastic/repository/ElasticRepositoryImplUnitTest.java
+++ src/test/java/com/t3t/bookstoreapi/elastic/repository/ElasticRepositoryImplUnitTest.java
@@ -1,10 +1,6 @@
 package com.t3t.bookstoreapi.elastic.repository;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-
 import com.t3t.bookstoreapi.elastic.model.dto.ElasticDocument;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,11 +11,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
-import org.springframework.data.elasticsearch.core.query.Query;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ElasticRepositoryImplUnitTest {
 
@@ -55,29 +54,6 @@ public class ElasticRepositoryImplUnitTest {
     }
 
     @Test
-    @DisplayName("모든 검색 유형을 사용한 통합 일치하는 도서 검색 테스트")
-    void testFindByAllWithKeywordMatch() {
-        // 첫 번째 호출에 대한 설정: 검색 결과가 없음
-        SearchHits<ElasticDocument> firstCallResult = mock(SearchHits.class);
-        when(firstCallResult.hasSearchHits()).thenReturn(false);
-
-        // 두 번째 호출에 대한 설정: 검색 결과가 있음
-        SearchHits<ElasticDocument> secondCallResult = mock(SearchHits.class);
-        when(secondCallResult.hasSearchHits()).thenReturn(true);
-
-        // elasticsearchOperations.search 호출 시 첫 번째와 두 번째 반환값 설정
-        when(elasticsearchOperations.search(any(NativeSearchQuery.class), eq(ElasticDocument.class)))
-                .thenReturn(firstCallResult, secondCallResult); // 첫 번째 호출과 두 번째 호출에 대한 반환값 설정
-
-        // 테스트 실행
-        SearchHits<ElasticDocument> result = elasticRepositoryImpl.findByAll("test query", Pageable.unpaged());
-
-        // 검증
-        assertTrue(result.hasSearchHits()); // 두 번째 호출 결과에서 검색 결과가 있음을 확인
-        verify(elasticsearchOperations, times(2)).search(any(NativeSearchQuery.class), eq(ElasticDocument.class)); // search 메소드가 정확히 두 번 호출되었는지 확인
-    }
-
-    @Test
     @DisplayName("도서 제목을 사용한 통합 일치하는 도서 검색 테스트")
     void testFindByBookNameWithExactMatch() {
         // 테스트 조건 설정: 정확한 일치 쿼리에 대한 결과가 있을 경우
@@ -91,29 +67,6 @@ public class ElasticRepositoryImplUnitTest {
         // 검증
         assertTrue(result.hasSearchHits());
         verify(elasticsearchOperations).search(any(NativeSearchQuery.class), eq(ElasticDocument.class));
-    }
-
-    @Test
-    @DisplayName("도서 제목을 통해 일치하는 항목이 없는 경우 유사한 도서 검색")
-    void testFindByBookNameWithKeywordMatch() {
-        // 첫 번째 호출에 대한 설정: 검색 결과가 없음
-        SearchHits<ElasticDocument> firstCallResult = mock(SearchHits.class);
-        when(firstCallResult.hasSearchHits()).thenReturn(false);
-
-        // 두 번째 호출에 대한 설정: 검색 결과가 있음
-        SearchHits<ElasticDocument> secondCallResult = mock(SearchHits.class);
-        when(secondCallResult.hasSearchHits()).thenReturn(true);
-
-        // elasticsearchOperations.search 호출 시 첫 번째와 두 번째 반환값 설정
-        when(elasticsearchOperations.search(any(NativeSearchQuery.class), eq(ElasticDocument.class)))
-                .thenReturn(firstCallResult, secondCallResult); // 첫 번째 호출과 두 번째 호출에 대한 반환값 설정
-
-        // 테스트 실행
-        SearchHits<ElasticDocument> result = elasticRepositoryImpl.findByBookName("test query", Pageable.unpaged());
-
-        // 검증
-        assertTrue(result.hasSearchHits()); // 두 번째 호출 결과에서 검색 결과가 있음을 확인
-        verify(elasticsearchOperations, times(2)).search(any(NativeSearchQuery.class), eq(ElasticDocument.class)); // search 메소드가 정확히 두 번 호출되었는지 확인
     }
     @Test
     @DisplayName("출판사를 사용한 통합 일치하는 도서 검색 테스트")
@@ -130,29 +83,6 @@ public class ElasticRepositoryImplUnitTest {
         assertTrue(result.hasSearchHits());
         verify(elasticsearchOperations).search(any(NativeSearchQuery.class), eq(ElasticDocument.class));
     }
-
-    @Test
-    @DisplayName("출판사 검색을 통해 일치하는 항목이 없는 경우 유사한 도서 검색")
-    void testFindByPublisherWithKeywordMatch() {
-        // 첫 번째 호출에 대한 설정: 검색 결과가 없음
-        SearchHits<ElasticDocument> firstCallResult = mock(SearchHits.class);
-        when(firstCallResult.hasSearchHits()).thenReturn(false);
-
-        // 두 번째 호출에 대한 설정: 검색 결과가 있음
-        SearchHits<ElasticDocument> secondCallResult = mock(SearchHits.class);
-        when(secondCallResult.hasSearchHits()).thenReturn(true);
-
-        // elasticsearchOperations.search 호출 시 첫 번째와 두 번째 반환값 설정
-        when(elasticsearchOperations.search(any(NativeSearchQuery.class), eq(ElasticDocument.class)))
-                .thenReturn(firstCallResult, secondCallResult); // 첫 번째 호출과 두 번째 호출에 대한 반환값 설정
-
-        // 테스트 실행
-        SearchHits<ElasticDocument> result = elasticRepositoryImpl.findByPublisher("test query", Pageable.unpaged());
-
-        // 검증
-        assertTrue(result.hasSearchHits()); // 두 번째 호출 결과에서 검색 결과가 있음을 확인
-        verify(elasticsearchOperations, times(2)).search(any(NativeSearchQuery.class), eq(ElasticDocument.class)); // search 메소드가 정확히 두 번 호출되었는지 확인
-    }
     @Test
     @DisplayName("참여자를 사용한 통합 일치하는 도서 검색 테스트")
     void testFindByAuthorNameWithExactMatch() {
@@ -168,28 +98,66 @@ public class ElasticRepositoryImplUnitTest {
         assertTrue(result.hasSearchHits());
         verify(elasticsearchOperations).search(any(NativeSearchQuery.class), eq(ElasticDocument.class));
     }
-
     @Test
-    @DisplayName("참여자 검색을 통해 일치하는 항목이 없는 경우 유사한 도서 검색")
-    void testFindByAuthorNameWithKeywordMatch() {
-        // 첫 번째 호출에 대한 설정: 검색 결과가 없음
-        SearchHits<ElasticDocument> firstCallResult = mock(SearchHits.class);
-        when(firstCallResult.hasSearchHits()).thenReturn(false);
-
-        // 두 번째 호출에 대한 설정: 검색 결과가 있음
-        SearchHits<ElasticDocument> secondCallResult = mock(SearchHits.class);
-        when(secondCallResult.hasSearchHits()).thenReturn(true);
-
-        // elasticsearchOperations.search 호출 시 첫 번째와 두 번째 반환값 설정
+    @DisplayName("모든 검색 유형을 사용한 통합 일치하는 도서 검색 테스트")
+    void testFindByAllWithCategoryExactMatch() {
+        // 테스트 조건 설정: 정확한 일치 쿼리에 대한 결과가 있을 경우
         when(elasticsearchOperations.search(any(NativeSearchQuery.class), eq(ElasticDocument.class)))
-                .thenReturn(firstCallResult, secondCallResult); // 첫 번째 호출과 두 번째 호출에 대한 반환값 설정
+                .thenReturn(searchHits);
+        when(searchHits.hasSearchHits()).thenReturn(true);
 
         // 테스트 실행
-        SearchHits<ElasticDocument> result;
-        result = elasticRepositoryImpl.findByAuthorName("test query", Pageable.unpaged());
+        SearchHits<ElasticDocument> result = elasticRepositoryImpl.findByAllCategory("test query",new BigDecimal("64"), Pageable.unpaged());
 
         // 검증
-        assertTrue(result.hasSearchHits()); // 두 번째 호출 결과에서 검색 결과가 있음을 확인
-        verify(elasticsearchOperations, times(2)).search(any(NativeSearchQuery.class), eq(ElasticDocument.class)); // search 메소드가 정확히 두 번 호출되었는지 확인
+        assertTrue(result.hasSearchHits());
+        verify(elasticsearchOperations).search(any(NativeSearchQuery.class), eq(ElasticDocument.class));
     }
+
+    @Test
+    @DisplayName("도서 제목을 사용한 통합 일치하는 도서 검색 테스트")
+    void testFindByBookNameCategoryWithExactMatch() {
+        // 테스트 조건 설정: 정확한 일치 쿼리에 대한 결과가 있을 경우
+        when(elasticsearchOperations.search(any(NativeSearchQuery.class), eq(ElasticDocument.class)))
+                .thenReturn(searchHits);
+        when(searchHits.hasSearchHits()).thenReturn(true);
+
+        // 테스트 실행
+        SearchHits<ElasticDocument> result = elasticRepositoryImpl.findByBookNameCategory("test query",new BigDecimal("64"), Pageable.unpaged());
+
+        // 검증
+        assertTrue(result.hasSearchHits());
+        verify(elasticsearchOperations).search(any(NativeSearchQuery.class), eq(ElasticDocument.class));
+    }
+    @Test
+    @DisplayName("출판사를 사용한 통합 일치하는 도서 검색 테스트")
+    void testFindByPublisherCategoryWithExactMatch() {
+        // 테스트 조건 설정: 정확한 일치 쿼리에 대한 결과가 있을 경우
+        when(elasticsearchOperations.search(any(NativeSearchQuery.class), eq(ElasticDocument.class)))
+                .thenReturn(searchHits);
+        when(searchHits.hasSearchHits()).thenReturn(true);
+
+        // 테스트 실행
+        SearchHits<ElasticDocument> result = elasticRepositoryImpl.findByPublisherCategory("test query",new BigDecimal("64"), Pageable.unpaged());
+
+        // 검증
+        assertTrue(result.hasSearchHits());
+        verify(elasticsearchOperations).search(any(NativeSearchQuery.class), eq(ElasticDocument.class));
+    }
+    @Test
+    @DisplayName("참여자를 사용한 통합 일치하는 도서 카테고리검색 테스트")
+    void testFindByAuthorNameCategoryWithExactMatch() {
+        // 테스트 조건 설정: 정확한 일치 쿼리에 대한 결과가 있을 경우
+        when(elasticsearchOperations.search(any(NativeSearchQuery.class), eq(ElasticDocument.class)))
+                .thenReturn(searchHits);
+        when(searchHits.hasSearchHits()).thenReturn(true);
+
+        // 테스트 실행
+        SearchHits<ElasticDocument> result = elasticRepositoryImpl.findByAuthorNameCategory("test query",new BigDecimal("64"), Pageable.unpaged());
+
+        // 검증
+        assertTrue(result.hasSearchHits());
+        verify(elasticsearchOperations).search(any(NativeSearchQuery.class), eq(ElasticDocument.class));
+    }
+
 }
