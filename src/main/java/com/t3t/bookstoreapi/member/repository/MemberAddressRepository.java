f59e3bef327b8861b7cf126df99ee9c24b2019b1
woody35545 <woody35545@gmail.com>
2024-05-06 22:48:10 +0900

    feat: `MemberAddressRepository` - 회원 주소 식별자로 조회

--- src/main/java/com/t3t/bookstoreapi/member/repository/MemberAddressRepository.java
+++ src/main/java/com/t3t/bookstoreapi/member/repository/MemberAddressRepository.java
@@ -3,6 +3,9 @@ package com.t3t.bookstoreapi.member.repository;
 import com.t3t.bookstoreapi.member.model.entity.MemberAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberAddressRepository extends JpaRepository<MemberAddress, Long>, MemberAddressRepositoryCustom {
     int countByMemberId(long memberId);
+    List<MemberAddress> findByMemberId(long memberId);
 }
