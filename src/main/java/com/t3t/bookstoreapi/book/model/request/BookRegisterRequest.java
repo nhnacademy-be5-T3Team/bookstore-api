8ca6cbda8cdd0bcfa1ba3a5d04d916a758b9883a
yujinKim <yujinkimvv@gmail.com>
2024-05-07 20:59:57 +0900

    feat : #140 도서 등록 요청 dto validation 추가

--- src/main/java/com/t3t/bookstoreapi/book/model/request/BookRegisterRequest.java
+++ src/main/java/com/t3t/bookstoreapi/book/model/request/BookRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.t3t.bookstoreapi.book.model.request;
 
+import com.t3t.bookstoreapi.book.annotation.UniqueParticipantMap;
 import com.t3t.bookstoreapi.book.model.dto.ParticipantMapDto;
 import lombok.Builder;
 import lombok.Data;
@@ -20,9 +21,11 @@ import java.util.List;
 @Builder
 public class BookRegisterRequest {
     @NotBlank(message = "도서 제목을 입력해주세요.")
+    @Size(max = 255, message = "도서 제목은 최대 255자여야 합니다.")
     private String bookTitle; // 도서 제목
 
     @NotBlank(message = "도서 ISBN을 입력해주세요.")
+    @Pattern(regexp = "\\d{13}", message = "도서 ISBN은 13자리의 숫자여야 합니다.")
     private String bookIsbn; // 도서 isbn
 
     @NotNull(message = "도서 가격을 입력해주세요.")
@@ -34,6 +37,8 @@ public class BookRegisterRequest {
     private BigDecimal bookDiscountRate; // 도서 할인율
 
     @NotNull(message = "포장 가능 여부를 입력해주세요.")
+    @Min(value = 0, message = "포장 가능 여부는 0(불가능) 또는 1(가능)만 입력할 수 있습니다.")
+    @Max(value = 1, message = "포장 가능 여부는 0(불가능) 또는 1(가능)만 입력할 수 있습니다.")
     private Integer packagingAvailableStatus; // 포장 가능 여부
 
     @NotNull(message = "도서 출판일을 입력해주세요.")
@@ -54,6 +59,7 @@ public class BookRegisterRequest {
     private Long publisherId; // 출판사 id
 
     @NotEmpty(message = "도서 참여자를 선택해주세요.")
+    @UniqueParticipantMap
     private List<ParticipantMapDto> participantMapList; // 도서 참여자 - 참여자 역할 선택 리스트
 
     @NotNull(message = "도서 썸네일을 선택해주세요.")
@@ -62,8 +68,10 @@ public class BookRegisterRequest {
     private List<MultipartFile> bookImageList; // 도서 미리보기 이미지
 
     @NotEmpty(message = "카테고리를 선택해주세요.")
+    @Size(max = 10, message = "최대 10개의 카테고리를 선택할 수 있습니다.")
     private List<Integer> categoryList; // 카테고리 id 리스트
 
     @NotEmpty(message = "태그를 선택해주세요.")
+    @Size(max = 10, message = "최대 10개의 태그를 선택할 수 있습니다.")
     private List<Long> tagList; // 태그 id 리스트
 }
