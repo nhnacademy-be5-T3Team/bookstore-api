ec156cf6514e26abe227e7e5cea07e68fc8a9b80
woody35545 <woody35545@gmail.com>
2024-05-08 16:59:42 +0900

    feat: #162 `CertCodeService` - 인증 코드 발급 기능

---
+++ src/main/java/com/t3t/bookstoreapi/certcode/service/CertCodeService.java
@@ -0,0 +1,47 @@
+package com.t3t.bookstoreapi.certcode.service;
+
+import com.nhn.dooray.client.DoorayHook;
+import com.nhn.dooray.client.DoorayHookSender;
+import com.t3t.bookstoreapi.certcode.entity.CertCode;
+import com.t3t.bookstoreapi.certcode.repository.CertCodeRepository;
+import com.t3t.bookstoreapi.member.exception.MemberNotFoundForIdException;
+import com.t3t.bookstoreapi.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.SecureRandom;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CertCodeService {
+    private final MemberRepository memberRepository;
+    private final DoorayHookSender messageSender;
+    private final CertCodeRepository certCodeRepository;
+
+    /**
+     * 인증 코드 발급
+     * @param memberId 인증 코드를 발급할 회원 식별자
+     * @author wooody35545(구건모)
+     */
+    public void issueMemberActivationCertCode(long memberId) {
+
+        if (memberRepository.existsById(memberId)) {
+            throw new MemberNotFoundForIdException(memberId);
+        }
+
+        CertCode certCode = certCodeRepository.save(CertCode.builder()
+                .memberId(memberId)
+                .code(String.format("%05d", new SecureRandom().nextInt(100000)))
+                .build());
+
+        messageSender.send(DoorayHook.builder()
+                .botName("t3t-cert-bot")
+                .text(new StringBuilder().append("[t3t-bookstore]\n")
+                        .append("회원님의 휴면 계정 활성화 인증 코드는 ")
+                        .append(certCode.getCode())
+                        .append(" 입니다.").toString())
+                .build());
+    }
+}
