de11a7951093bdad7f75d013c97bcc46dce2d5a8
PARKJONGGYEONG <gd30220@o.cnu.ac.kr>
2024-05-10 17:03:14 +0900

    feat: #101 자동완성 구현

--- src/main/java/com/t3t/bookstoreapi/elastic/service/AutocompleteService.java
+++ src/main/java/com/t3t/bookstoreapi/elastic/service/AutocompleteService.java
@@ -1,53 +1,50 @@
 package com.t3t.bookstoreapi.elastic.service;
 
+import com.t3t.bookstoreapi.elastic.repository.ElasticRepository;
+import com.t3t.bookstoreapi.elastic.util.Constants;
 import com.t3t.bookstoreapi.model.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.suggest.SuggestBuilder;
 import org.elasticsearch.search.suggest.SuggestBuilders;
 import org.elasticsearch.search.suggest.completion.CompletionSuggestionBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class AutocompleteService {
-
     private final RestHighLevelClient client;
-    public BaseResponse<List<String>> autocomplete(String prefix) throws IOException {
+    private final ElasticRepository elasticRepository;
 
-        SearchRequest searchRequest = new SearchRequest("t3t_book"); // Elasticsearch 인덱스 이름
+    public BaseResponse<List<String>> autocomplete(String prefix) throws IOException {
+        SearchRequest searchRequest = new SearchRequest("t3t_book");
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 
-        CompletionSuggestionBuilder completionSuggestionBuilder =
-                SuggestBuilders.completionSuggestion("book_name.book_name2") // 자동완성 필드 설정
-                        .prefix(prefix)
-                        .size(10);
-
-        searchSourceBuilder.suggest(new SuggestBuilder().addSuggestion("suggest_book_name", completionSuggestionBuilder));
+        BoolQueryBuilder boolQuery = elasticRepository.autocomplete(prefix);
+        searchSourceBuilder.query(boolQuery);
         searchRequest.source(searchSourceBuilder);
 
         SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-        return parseSuggestResponse(response, "suggest_book_name");
-
+        return parseSuggestResponse(response);
     }
 
-
-    private BaseResponse<List<String>> parseSuggestResponse(SearchResponse response, String suggestionName) {
-        List<String> suggestions = response.getSuggest().getSuggestion(suggestionName).getEntries().stream()
-                .flatMap(entry -> entry.getOptions().stream())
-                .map(option -> option.getText().string())
-                .collect(Collectors.toList());
-
-        BaseResponse<List<String>> baseResponse = new BaseResponse<>();
-        baseResponse.setData(suggestions);
-        return baseResponse;
+    private BaseResponse<List<String>> parseSuggestResponse(SearchResponse response) {
+        List<String> suggestions = new ArrayList<>();
+        for (SearchHit hit : response.getHits().getHits()) {
+            suggestions.add((String) hit.getSourceAsMap().get("book_name"));
+        }
+        return new BaseResponse<>("book_name",suggestions);
     }
 }
