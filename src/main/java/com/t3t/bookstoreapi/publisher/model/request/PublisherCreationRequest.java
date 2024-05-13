1af8dfda8fb924c94a0f908985c83bf9df728873
hydrationn <suhwaparkk1031@gmail.com>
2024-05-13 12:57:32 +0900

    docs: #72 출판사 JavaDoc 작성

--- src/main/java/com/t3t/bookstoreapi/publisher/model/request/PublisherCreationRequest.java
+++ src/main/java/com/t3t/bookstoreapi/publisher/model/request/PublisherCreationRequest.java
@@ -7,6 +7,12 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
+/**
+ * 출판사 상세 정보 생성을 위한 요청 데이터 모델
+ * 관리자가 출판사 상세 정보를 생성할 때 필요한 정보를 전달하는 데 사용
+ *
+ * @author hydrationn(박수화)
+ */
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
