59110312003d6176b848dd30fa11b7336ad1d0b2
joohyun <hbk9093@koreatech.ac.kr>
2024-05-15 14:26:14 +0900

    fix:#160 생성자 추가

--- src/main/java/com/t3t/bookstoreapi/member/model/request/CouponDetailRequest.java
+++ src/main/java/com/t3t/bookstoreapi/member/model/request/CouponDetailRequest.java
@@ -3,10 +3,10 @@ package com.t3t.bookstoreapi.member.model.request;
 import lombok.*;
 
 @Builder
-@Getter
-@ToString
+@Getter@ToString
 @EqualsAndHashCode
 @NoArgsConstructor
+@AllArgsConstructor
 public class CouponDetailRequest {
     private String couponId;
 }
