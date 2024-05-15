221f3a9316cb959dfd6d88b48157533680b96cb0
woody35545 <woody35545@gmail.com>
2024-05-15 22:20:58 +0900

    feat: #180 `OrderController` - 특정 회원의 모든 주문 관련 정보를 페이징을 통해 조회하는 API

--- src/main/java/com/t3t/bookstoreapi/order/controller/OrderController.java
+++ src/main/java/com/t3t/bookstoreapi/order/controller/OrderController.java
@@ -7,10 +7,14 @@ import com.t3t.bookstoreapi.order.model.request.GuestOrderPreparationRequest;
 import com.t3t.bookstoreapi.order.model.response.GuestOrderPreparationResponse;
 import com.t3t.bookstoreapi.order.model.response.MemberOrderPreparationResponse;
 import com.t3t.bookstoreapi.order.model.response.OrderDetailInfoResponse;
+import com.t3t.bookstoreapi.order.model.response.OrderInfoResponse;
 import com.t3t.bookstoreapi.order.service.OrderDetailService;
+import com.t3t.bookstoreapi.order.service.OrderService;
 import com.t3t.bookstoreapi.order.service.OrderServiceFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +26,7 @@ import java.util.List;
 public class OrderController {
     private final OrderDetailService orderDetailService;
     private final OrderServiceFacade orderServiceFacade;
+    private final OrderService orderService;
 
     /**
      * 주문 내에 속해있는 주문 상세 리스트 조회
@@ -77,5 +82,15 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     public BaseResponse<MemberOrderPreparationResponse> createMemberOrder(@RequestBody MemberOrderPreparationRequest request) {
         return new BaseResponse<MemberOrderPreparationResponse>().data(orderServiceFacade.prepareOrder(request));
+    }
+
+    /**
+     * 특정 회원의 모든 주문 관련 정보를 페이징을 통해 조회
+     *
+     * @author woody35545(구건모)
+     */
+    @GetMapping("/members/{memberId}/orders")
+    public BaseResponse<Page<OrderInfoResponse>> getMemberOrderInfoListByMemberId(@PathVariable("memberId") Long memberId, Pageable pageable) {
+        return new BaseResponse<Page<OrderInfoResponse>>().data(orderService.getMemberOrderInfoListByMemberId(memberId, pageable));
     }
 }
